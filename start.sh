57b2b0c013f67899ab81c79c662cf9301cdcdf6e
BirdZhang <0312birdzhang@gmail.com>
2018-07-09 17:08:22 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,13 +2,12 @@
 mkdir -p /data
 cd /data
 /usr/bin/createrepo -pdo /data/ /data/
-#/usr/bin/python -m SimpleHTTPServer 10088 &
 /etc/init.d/nginx start
 /usr/bin/inotifywait -mrq  --format '%w%f %e' \
         -e modify,create,delete --exclude '^/data/(.repodata|.olddata|repodata|.repodata/.*)' /data \
         | while read x
         do
-	  file_action="$x"
+          file_action="$x"
           file=`echo $file_action|awk '{print $1}'`
           action=`echo $file_action|awk '{print $2}'` 
           if [[ "$file" =~ "repodata" ]]
